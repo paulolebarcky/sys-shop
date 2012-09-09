2aa1c193bbadd626d751430fcb842a8c24ef84bd
paulo <paulo@localhost>
2012-09-09 16:43:49 -0300

    Criado classe controladora para tratar funções genéricas de crud.

---
+++ src/sys/shop/controller/PrincipalController.java
@@ -0,0 +1,122 @@
+package sys.shop.controller;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import sys.shop.controller.exceptions.NonexistentEntityException;
+
+/**
+ * Classe abstrata para tratar funções genéricas de crud.
+ *
+ * @author paulo
+ */
+public abstract class PrincipalController<T> extends PersistenceManager implements Serializable {
+
+    private T t;
+
+    public PrincipalController(T t) {
+        this.t = t;
+    }
+
+    public void create() throws Exception {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            em.persist(this.t);
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void edit() throws NonexistentEntityException, Exception {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            em.merge(this.t);
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void remove(Integer id) throws NonexistentEntityException {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            Object object;
+            try {
+                object = em.getReference(this.t.getClass(), id);
+            } catch (EntityNotFoundException enfe) {
+                throw new NonexistentEntityException("The object " + this.t.getClass() + " with id " + id + " no longer exists.", enfe);
+            }
+            em.remove(object);
+            em.getTransaction().commit();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    private List<T> findEntities(boolean all, int maxResults, int firstResult) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(this.t.getClass()));
+            Query q = em.createQuery(cq);
+            if (!all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            return q.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public List<T> findEntities() {
+        return findEntities(true, -1, -1);
+    }
+
+    public List<T> findEntities(int maxResults, int firstResult) {
+        return findEntities(false, maxResults, firstResult);
+    }
+
+    public Object find(Integer id) {
+        EntityManager em = getEntityManager();
+        try {
+            return em.find(this.t.getClass(), id);
+        } finally {
+            em.close();
+        }
+    }
+
+    public int getCount() {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            Root<T> rt = cq.from(this.t.getClass());
+            cq.select(em.getCriteriaBuilder().count(rt));
+            Query q = em.createQuery(cq);
+            return ((Long) q.getSingleResult()).intValue();
+        } finally {
+            em.close();
+        }
+    }
+}
